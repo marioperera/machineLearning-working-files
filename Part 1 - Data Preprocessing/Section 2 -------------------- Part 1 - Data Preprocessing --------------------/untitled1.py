8d9c745e64a4ed8a9bd597dfb1ec29d78186cf98
marioperera <pereramario19@gmail.com>
2019-01-17 00:24:16 +0530

    this is the test set for udemy ml course

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/untitled1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 14 09:54:31 2019
+
+@author: aztek
+"""
+
---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/linear_regression.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 23:43:41 2019
+
+@author: aztek
+"""
+
